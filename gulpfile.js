51fb65029b8657a13d2e77a3776d2e8d98331a4d
Victor Nicolini de Queiroz Santos <victor.nicolinivnqs@gmail.com>
2025-04-23 20:46:49 -0300

    firt teste

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,21 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
 
 function styles() {
-    return gulp.src('./src/styles/*.scss')
-        .pipe(sass({ outputStyles: 'compressed'}))
+    return gulp.src('./src/styles/*.scss') // Retorna a stream
+        .pipe(sass({ outputStyle: 'compressed' }))
         .pipe(gulp.dest('./dist/css'));
 }
 
-exports.default = styles;
-exports.watch = function () {
-    gulp.watch('./src/styles/*.scss', ('styles', gulp.parallel(styles)));
+function images() {
+    return gulp.src('./src/images/**/*') // Retorna a stream
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/images'));
 }
+
+exports.default = gulp.parallel(styles, images);
+
+exports.watch = function () {
+    gulp.watch('./src/styles/*.scss', styles); // Corrige a chamada da tarefa
+};
