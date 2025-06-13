37118246c55eeec327e4782bd882e89c1ba4f4d5
Victor Nicolini de Queiroz Santos <victor.nicolinivnqs@gmail.com>
2025-06-12 21:19:40 -0300

    modificando caminho do script

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,13 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const imagemin = require('gulp-imagemin');
+const uglify = require('gulp-uglify');
+
+function scripts() {
+    return gulp.src('./src/scripts/*.js') // Retorna a stream
+        .pipe(uglify()) // Minifica os arquivos JavaScript
+        .pipe(gulp.dest('./dist/js')); // Salva os arquivos minificados na pasta dist/js
+}   
 
 function styles() {
     return gulp.src('./src/styles/*.scss') // Retorna a stream
@@ -14,8 +21,9 @@ function images() {
         .pipe(gulp.dest('./dist/images'));
 }
 
-exports.default = gulp.parallel(styles, images);
+exports.default = gulp.parallel(styles, images, scripts);
 
 exports.watch = function () {
-    gulp.watch('./src/styles/*.scss', styles); // Corrige a chamada da tarefa
+    gulp.watch('./src/styles/*.scss', gulp.parallel(styles)); // Corrige a chamada da tarefa
+    gulp.watch('./src/scripts/*.js',  gulp.parallel(scripts)); // Corrige a chamada da tarefa
 };
